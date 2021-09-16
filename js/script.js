9e4dc0a3dc385d2e41d507a81968fcdd02c9d16d
LearnGitHub <thanhnd311020@gmail.com>
2021-09-17 00:32:41 +0700

    update project

--- js/script.js
+++ js/script.js
@@ -20,10 +20,10 @@ window.onscroll = () => {
     if (window.scrollY > 400) {
         nav.classList.add('fix-nav');
         console.log("ok");
-    }else if(window.screenY == 0 ){
-       
+    } else if (window.screenY == 0) {
+
         nav.classList.remove('fix-nav');
-        
+
     }
 }
 
@@ -40,5 +40,38 @@ window.onscroll = () => {
 //     console.log("alo");
 // }
 
+//  time deal
 
+let countDate = new Date('2021-09-16 21:58:10').getTime(); // xuất ra dãy số đại diện ngày tháng
+console.log(countDate);
+function CountDown() {
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    if (gap > 0) {
+        var d = Math.floor(gap / (day));
+        var h = Math.floor((gap % (day)) / (hour));
+        var m = Math.floor((gap % (hour)) / (minute));
+        var s = Math.floor((gap % (minute)) / (second));
+    } else {
+        var d = '00';
+        var h = '00';
+        var m = '00';
+        var s = '00';
+    }
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+setInterval(function () {
+    CountDown();
+}, 1000)
 

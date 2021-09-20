c74c26a125823bf7e4c91fa04c826e62c2c3dd39
LearnGitHub <thanhnd311020@gmail.com>
2021-09-20 23:31:20 +0700

    update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 var nav = document.querySelector('#navx');
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
 }
 // nav.addEventListener("scroll" , event =>{
 //     var x = nav.scrollTop();
@@ -14,41 +14,57 @@ menu.onclick = () => {
 // })
 
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
 
-    if (window.scrollY > 400) {
-        nav.classList.add('fix-nav');
-        console.log("ok");
-    } else if (window.screenY == 0) {
+  if (window.scrollY > 400) {
+    nav.classList.add('fix-nav');
+    console.log("ok");
+  } else if (window.screenY == 0) {
 
-        nav.classList.remove('fix-nav');
+    nav.classList.remove('fix-nav');
 
-    }
+  }
 
-    
-    if(window.scrollY > 60){
-        document.querySelector('#scroll-top').classList.add('active');
-    }else{
-        document.querySelector('#scroll-top').classList.remove('active');
-    }
+
+  if (window.scrollY > 60) {
+    document.querySelector('#scroll-top').classList.add('active');
+  } else {
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
 }
 
 var payment = document.querySelector('.payment');
 
-function paymentBy(){
-//    document.getElementById('notecod').style.display = "block";
-    if(document.getElementById('paymentcod').checked ==true){
-        document.getElementById('notecod').style.display = "block";
-        document.getElementById('paypalbox').style.display = "none";
-    }
-     if(document.getElementById('paymentpaypal').checked ==true){
-        document.getElementById('paypalbox').style.display = "block";
-        document.getElementById('notecod').style.display = "none";
-    }
-//    alert("ok")
+function paymentBy() {
+  //    document.getElementById('notecod').style.display = "block";
+  if (document.getElementById('paymentcod').checked == true) {
+    document.getElementById('notecod').style.display = "block";
+    document.getElementById('paypalbox').style.display = "none";
+  }
+  if (document.getElementById('paymentpaypal').checked == true) {
+    document.getElementById('paypalbox').style.display = "block";
+    document.getElementById('notecod').style.display = "none";
+  }
+  //    alert("ok")
 }
 
+//  Quantity 
+var myInput = document.getElementById('quantity');
+function stepper(btn) {
+  let id = btn.getAttribute("id");
+  let min = myInput.getAttribute("min");
+  let max = myInput.getAttribute("max");
+  let step = myInput.getAttribute("step");
+  let val = myInput.getAttribute("value");
+  // alert("ok")
+  let calcStep = (id == "increment") ? (step * 1) : (step * - 1)
+  let newValue = parseInt(val) + calcStep;
+  if (newValue >= min && newValue <= max) {
+    myInput.setAttribute("value", newValue);
+  }
+  // console.log(id, calcStep);
+}
 
 
 // nav.onscroll = () =>{
@@ -68,84 +84,86 @@ let countDate = new Date('2021-10-16 21:58:10').getTime(); // xuất ra dãy s�
 console.log(countDate);
 function CountDown() {
 
-    let now = new Date().getTime();
-    gap = countDate - now;
+  let now = new Date().getTime();
+  gap = countDate - now;
 
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
 
-    if (gap > 0) {
-        var d = Math.floor(gap / (day));
-        var h = Math.floor((gap % (day)) / (hour));
-        var m = Math.floor((gap % (hour)) / (minute));
-        var s = Math.floor((gap % (minute)) / (second));
-    } else {
-        var d = '00';
-        var h = '00';
-        var m = '00';
-        var s = '00';
-    }
-    document.getElementById('day').innerText = d;
-    document.getElementById('hour').innerText = h;
-    document.getElementById('minute').innerText = m;
-    document.getElementById('second').innerText = s;
+  if (gap > 0) {
+    var d = Math.floor(gap / (day));
+    var h = Math.floor((gap % (day)) / (hour));
+    var m = Math.floor((gap % (hour)) / (minute));
+    var s = Math.floor((gap % (minute)) / (second));
+  } else {
+    var d = '00';
+    var h = '00';
+    var m = '00';
+    var s = '00';
+  }
+  document.getElementById('day').innerText = d;
+  document.getElementById('hour').innerText = h;
+  document.getElementById('minute').innerText = m;
+  document.getElementById('second').innerText = s;
 }
 
 setInterval(function () {
-    CountDown();
+  CountDown();
 }, 1000)
 
 
 
 
 var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 200000,
-      disableOnInteraction: false,
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 200000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    loop:true,
-    breakpoints: {
-      0:{
-        slidesPerView: 1,
-      },
-      640: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1366: {
-        slidesPerView: 3,
-      },
+    640: {
+      slidesPerView: 2,
     },
-  });
+    768: {
+      slidesPerView: 2,
+    },
+    1366: {
+      slidesPerView: 3,
+    },
+  },
+});
 var swiper = new Swiper(".deal_product", {
-    spaceBetween: 30,
-    slidesPerView: 4,
-    centeredSlides: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
+  spaceBetween: 30,
+  // slidesPerView: 4,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    loop:true,
-    // breakpoints: {
-    //   0:{
-    //     slidesPerView: 2,
-    //   },
-    //   640: {
-    //     slidesPerView: 3,
-    //   },
-    //   768: {
-    //     slidesPerView: 4,
-    //   },
-    //   1366: {
-    //     slidesPerView: 3,
-    //   },
-    // },
-  });
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1366: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+
 
 
